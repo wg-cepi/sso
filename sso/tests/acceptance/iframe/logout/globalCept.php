14cca09623d6e2737bb42b1fbc00092c8ca89a86
wg-cepi <wgssogithub@gmail.com>
2016-02-21 00:38:34 +0100

    acceptance test tweak, config error fix

--- sso/tests/acceptance/iframe/logout/globalCept.php
+++ sso/tests/acceptance/iframe/logout/globalCept.php
@@ -5,12 +5,14 @@ require PROJECT_ROOT . '/sso/tests/acceptance/iframe/login/successCept.php';
 
 $I->click('Global logout');
 $I->amOnPage('/index.php?f=2');
+$I->wait(1);
 $I->switchToIFrame("id-iframe-login");
 $I->see('Login to Webgarden SSO');
 $I->switchToIFrame();
 
 //check if user can't "Continue as ..."
 $I->amOnUrl('http://domain2.local/?f=2');
+$I->wait(1);
 $I->switchToIFrame("id-iframe-login");
 $I->see('Email');
 $I->see('Password');
